be52a7ad546032d81c193c6c81eac6c9b8c93791
wrmf <31448920+wrmf@users.noreply.github.com>
2023-02-09 07:57:31 -0600

    part 1

--- CS3203_Assignment3.rb
+++ CS3203_Assignment3.rb
@@ -1,7 +1,16 @@
 def add_nums(array)
-  temp = 0
+  temp = 0 #start at 0 for addition
   array.each do |num|
-    temp += num
+    temp += num #loop through array and add
   end
-  temp
+  temp #return temp
 end
+
+def multiply_nums(array)
+  temp = 1 #start at 1 for multiplication
+  array.each do |num|
+    temp *= num #loop through array and multiply
+  end
+  temp #return temp
+end
+
